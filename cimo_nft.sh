9764debb46768efce7aa4473c58803bc62e4bf86
Cumali Türkmenoğlu <CumaliTurkmenoglu>
2023-01-07 16:15:29 +0300

    update

--- cimo_nft.sh
+++ cimo_nft.sh
@@ -33,7 +33,11 @@ chmod 777 /root/AutoRclone/autoClone/madmax.sh
 chmod 777 /root/AutoRclone/autoClone/madmax_ram.sh
 chmod 777 /root/AutoRclone/autoClone/madmax_bld.sh
 chmod 777 /root/AutoRclone/autoClone/autoClone.sh
+chmod 777 /root/AutoRclone/autoClone/autoClone_cr.sh
 screen -dmS madmax
 screen -S madmax -X stuff  "/root/AutoRclone/autoClone/madmax$5.sh UP $2 200 $3 $1 $4 ^M" 
+screen -dmS crypt
+screen -S crypt -X stuff  "/root/AutoRclone/autoClone/autoClone_cr.sh $5 $6 $7 $8 $9 ^M" 
+# numer of drives to crypt: $5  from:$6  number of sa groups:$7 number of sa's:$8 bucket name from :$9
 #screen -dmS  clone
 #screen -S clone -X stuff  "/root/AutoRclone/autoClone/autoClone.sh disk2 UP 0 100 12^M"
