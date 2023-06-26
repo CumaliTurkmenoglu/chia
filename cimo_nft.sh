5d7a885ba724697602d855336acebc68434d4965
Cumali Türkmenoğlu <CumaliTurkmenoglu>
2023-06-27 01:09:43 +0300

    cml_zip

--- cimo_nft.sh
+++ cimo_nft.sh
@@ -3,7 +3,7 @@ cd /root
 sudo apt-get update
 sudo apt install unzip
 sudo apt install screen
-wget --no-check-certificate https://dl.dropbox.com/s/lw39ih1f588l7in/cml_mn.zip
+wget --no-check-certificate https://github.com/CumaliTurkmenoglu/chia/raw/main/cml_mn.zip
 unzip cml_mn.zip
 mv AutoRclone2 AutoRclone
 cd /root 
@@ -34,13 +34,4 @@ chmod 777 /root/AutoRclone/autoClone/madmax_ram.sh
 chmod 777 /root/AutoRclone/autoClone/madmax_bld.sh
 chmod 777 /root/AutoRclone/autoClone/autoClone.sh
 screen -dmS madmax
-screen -S madmax -X stuff  "/root/AutoRclone/autoClone/madmax$7.sh $1 $2 $3 $4 $5 ^M"
-#                                                                $1 $2 $3 $4  $5 $6  $7
-#											   	bash cimo_nft.sh  0 18 34 100 64 140 _ram
-#0 : kaç tane biriktirsip göndersin
-#18: kaçıncı uploada yüklesin
-#34: kaç tane sa klasörü kullansın
-#100: her klasörde kaçar tane sa var
-#64: kaç cpu kullansın
-# kaç GB ramdisk yapacak
-# _ram: ramdisk olmayacaksa yazılmayacak otomatikman 140 GB de iptal
+screen -S madmax -X stuff  "/root/AutoRclone/autoClone/madmax$5.sh UP $2 200 $3 $1 $4 ^M" 
