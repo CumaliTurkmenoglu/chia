bfe8620228556971d3b07b0d225b6e5d542913f0
cumali.turkmenoglu <cumali.turkmenoglu@HITSOFT16>
2023-12-14 22:46:09 +0300

    ibm and gdrive are in seperate folders

--- ibm/cml_mn_ibm/AutoRclone2/autoClone/autoClone.sh
+++ ibm/cml_mn_ibm/AutoRclone2/autoClone/autoClone.sh
@@ -6,7 +6,7 @@ mkdir /disk2/plots2
 while :
 do
     echo "$remote -------"
-    rclone move /disk2/plots XCOSUP_$remote:  --exclude "/*.temp"  --transfers=$transfers  --drive-upload-cutoff 1000T --tpslimit 5 --drive-stop-on-upload-limit --drive-chunk-size 4096M --no-traverse --ignore-existing  -P -v
+    rclone move /disk2/plots XCOSUP_$remote:cml1  --exclude "/*.tmp"  --transfers=$transfers  --drive-upload-cutoff 1000T --tpslimit 5 --drive-stop-on-upload-limit --drive-chunk-size 4096M --no-traverse --ignore-existing  -P -v
 done
 
 #autoClone.sh 1 3
--- ibm/cml_mn_ibm/AutoRclone2/autoClone/madmax_ram.sh
+++ ibm/cml_mn_ibm/AutoRclone2/autoClone/madmax_ram.sh
@@ -8,46 +8,45 @@ accumulate=$5
 
 while true
 do
+        echo "donguuuu"
+
         MADMAX_PROCESS_ID=$(pgrep chia_plot)
         MADMAX_STATUS=$(ps -o s= -p $MADMAX_PROCESS_ID)
-        CLONE_PROCESS_ID=$(pgrep uploading_has_started)
-        CLONE_STATUS=$(ps -o s= -p $CLONE_PROCESS_ID)
         #S -> running
         #T -> SUSPENDED
 
-        if [ ! -z $MADMAX_PROCESS_ID ]
-            then
+        if [ ! -z "$MADMAX_PROCESS_ID" ]; then
                 PLOT_COUNT=$(ls /disk2/plots | wc -l) #plots klasörü içindeki taşınmaya hazır plot sayısına bak
 
-				if [ $clone == 0 ] && [ $PLOT_COUNT -ge $accumulate ] #clone processi mevcut değil ya da plotlar birikti
-				then
-						clone=1
-						#sudo su
-						echo "$2 adet plot birikti. Gönderelim"
-						chmod 777 /root/AutoRclone/autoClone/autoClone.sh
-						screen -dmS  clone
-						screen -S clone -X stuff  "/root/AutoRclone/autoClone/autoClone.sh $remote $transfer ^M"
-#						screen -S clone -X stuff  "/root/AutoRclone/autoClone/autoClone.sh $remote $transfer ^M"
-						sleep 5
-						screen -S clone -X stuff  "^C^M"
-						screen -S clone -X stuff  "sudo su^M"
-						sleep 3
-						screen -S clone -X stuff  "/root/AutoRclone/autoClone/autoClone.sh $remote $transfer ^M"
-						sleep 2
-				fi
+                                if [ $clone == 0 ] && [ $PLOT_COUNT -ge $accumulate ] #clone processi mevcut değil ya da plotlar birikti
+                                then
+                                                clone=1
+                                                #sudo su
+                                                echo "$2 adet plot birikti. Gönderelim"
+                                                chmod 777 /root/AutoRclone/autoClone/autoClone.sh
+                                                screen -dmS  clone
+                                                screen -S clone -X stuff  "/root/AutoRclone/autoClone/autoClone.sh $remote $transfer ^M"
+#                                               screen -S clone -X stuff  "/root/AutoRclone/autoClone/autoClone.sh $remote $transfer ^M"
+                                                sleep 5
+                                                screen -S clone -X stuff  "^C^M"
+                                                screen -S clone -X stuff  "sudo su^M"
+                                                sleep 3
+                                                screen -S clone -X stuff  "/root/AutoRclone/autoClone/autoClone.sh $remote $transfer ^M"
+                                                sleep 2
+                                fi
 
-                if [ $MADMAX_STATUS == "S" ] && [ $PLOT_COUNT -gt $accumulate ] #madmax proccessi mevcut ve plot üretimi var ise
+                if [ $MADMAX_STATUS == "S" ] && [ $PLOT_COUNT -gt 7 ] #madmax proccessi mevcut ve plot üretimi var ise
                 then
                         #7 adet plot var. madmax durdurmamız gerekiyor.
                         kill -STOP $MADMAX_PROCESS_ID
-                        echo "7 adet plot. madmax duraklatıldı"
+                        echo "7 adet plottan fazla birikti madmax duraklatıldı"
                         sleep 1
 
-                elif [ $MADMAX_STATUS == "T" ] && [ $PLOT_COUNT -lt $accumulate ]
+                elif [ $MADMAX_STATUS == "T" ] && [ $PLOT_COUNT -lt 7 ]
                 then
                         #madmax duraklatışmıştı. plot sayısı 4'e düştü. madmax tekrardan çalıştırılıyor
                         kill -CONT $MADMAX_PROCESS_ID
-                        echo "4 adet plot. madmax devam"
+                        echo "7 adet plottan az kaldı madmax devam"
                         sleep 1
                 fi
 
@@ -65,13 +64,14 @@ do
                         echo "FIRST TIME: Tavuklar Otluyor. durdu. Temp klasörleri boşaltılıp yeniden başlatılıyor."
                         rm -r /disk2/temp/*
                         rm -r /disk2/temp2/*
+                        rm -r /disk2/plots/*.tmp
                         sudo mkdir /disk2/temp
-						sudo mkdir /disk2/temp2
-						sudo mkdir /disk2/plots
+                        sudo mkdir /disk2/temp2
+                        sudo mkdir /disk2/plots
                         sudo chmod 777 /disk2/temp
                         sudo chmod 777 /disk2/temp2
                         sudo chmod 777 /disk2/plots
-                        sudo mount -t tmpfs -o size=$ram tmpfs /disk2/temp2/
+                        sudo mount -t tmpfs -o size=$ramG tmpfs /disk2/temp2/
                         rm -r /disk2/temp/*
 
                         screen -ls | awk -vFS='\t|[.]' '/tavuk/ {system("screen -S "$2" -X quit")}'
@@ -80,5 +80,5 @@ do
                         screen -S tavuk -X stuff "./build/chia_plot -n -1 -r $cpu  -t /disk2/temp/ -2 /disk2/temp2/ -d /disk2/plots/ -c xch1wxvh0lupku627a3qnnrggssr9ey4utcfmk87llvg94wgahemc2yq2u37za -f b87d02b56e479832e7414bd7a0d8c3b03f18947eb406d82c9b1de30fcbad012fb84fe7bbd0b335310bcfd344a8bd9e94 ^M"
         fi
 
-        sleep 60
+        sleep 6
 done
