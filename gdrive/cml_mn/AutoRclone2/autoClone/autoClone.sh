804168d5d2dac373cc81e0d18e0593c6b3112233
cumali.turkmenoglu <cumali.turkmenoglu@HITSOFT16>
2023-12-16 00:18:08 +0300

    ibm and gdrive are in seperate folders

--- gdrive/cml_mn/AutoRclone2/autoClone/autoClone.sh
+++ gdrive/cml_mn/AutoRclone2/autoClone/autoClone.sh
@@ -5,7 +5,7 @@ while :
 do
     mkdir /disk2/plots2
     echo "$remote -------"
-    rclone move /disk2/plots XCOSUP_$remote:  --exclude "/*.temp"  --transfers=$3  --drive-upload-cutoff 1000T --tpslimit 5 --drive-stop-on-upload-limit --drive-chunk-size 4096M --no-traverse --ignore-existing  -P -v
+    rclone move /disk2/plots XCOSUP_$remote:  --exclude "/*.temp"  --transfers=$3  --drive-upload-cutoff 1000T --tpslimit 10 --multi-thread-streams 64 --drive-stop-on-upload-limit --drive-chunk-size 4096M --no-traverse --ignore-existing  -P -v
 remote=$(($remote + 1))
     if [ $remote -gt $2 ]; then
         remote=$1
