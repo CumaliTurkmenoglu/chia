4ca1e9cfbb2f1d30dbf43dd1c7645940bd89e91f
cumali.turkmenoglu <cumali.turkmenoglu@HITSOFT16>
2024-01-22 16:03:50 +0300

    mm

--- gdrive/cimo_nft.sh
+++ gdrive/cimo_nft.sh
@@ -61,4 +61,4 @@ screen -S download -X stuff  "/root/AutoRclone/autoClone/gdown_control.sh ^M"
 
 screen -dmS madmax
 screen -S madmax -X stuff  "/root/AutoRclone/autoClone/madmax_$5.sh $1 $2 $3 $4 ^M"
-#screen -S madmax -X stuff  "/root/AutoRclone/autoClone/madmax$5.sh $1: drive(token) $2: transfers $3: kaç cpu, $4: ram miktarı^M"
+#screen -S madmax -X stuff  "/root/AutoRclone/autoClone/madmax$5.sh $1: from(token) $2: To(token/drive) $3: kaç cpu, $4: ram miktarı^M"
