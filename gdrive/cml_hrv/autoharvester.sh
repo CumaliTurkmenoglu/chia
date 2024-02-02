2959a52af5ff5fdf4a4b91bfdef1130a985740a7
cumali.turkmenoglu <cumali.turkmenoglu@HITSOFT16>
2024-02-02 17:41:58 +0300

    mm

--- gdrive/cml_hrv/autoharvester.sh
+++ gdrive/cml_hrv/autoharvester.sh
@@ -1,8 +1,7 @@
 apt-get update && apt-get upgrade -y
-sleep 2  # Bir sonraki ad?ma ge?meden ?nce sistem g?ncellemelerinin tamamlanmas?n? bekler
+sleep 2  
 
 cd
-git clone https://github.com/Chia-Network/chia-blockchain.git -b latest
 mkdir drives
 cd drives
 mkdir drive_$1
@@ -10,12 +9,12 @@ cd drive_$1
 
 start_value=$2
 end_value=$3
-# Perform the for loop
 for ((i=start_value; i<=end_value; i++)); do
     mkdir "bucket$i"
 done
 
 cd
+git clone https://github.com/Chia-Network/chia-blockchain.git -b latest
 
 apt install rclone sshpass -y
 sleep 2
@@ -36,6 +35,34 @@ screen -S mount -X stuff "/root/cml_hrv/mount.sh $1 $2 $3\n"
 sleep 60
 
 sshpass -p 'Serhildanroot.123' scp -o StrictHostKeyChecking=no -r root@5.161.224.217:/root/.chia/mainnet/config/ssl/ca /root/
-sleep 5  # scp komutunun tamamlanmas?n? ve dosyalar?n transferini bekler
+sleep 5  
+
+cd chia-blockchain/
+sh install.sh 
+. ./activate
+chia init -c /root/ca
+chia configure --set-log-level INFO
+chia configure --set-farmer-peer 5.161.224.217:8447
+. ./activate
+
+start_value=$2
+end_value=$3
+for ((i=start_value; i<=end_value; i++)); do
+    chia plots add -d /root/drives/drive_$1/bucket$i
+done
+
+
+deactivate
+
+screen -dmS chi
+screen -S chi -X stuff ". ./activate\n"
+screen -S chi -X stuff "chia start harvester -r\n"
+screen -S chi -X stuff "tail -f /root/.chia/mainnet/log/debug.log\n"
+
+sleep 60
+
+done
+
+
 
 
